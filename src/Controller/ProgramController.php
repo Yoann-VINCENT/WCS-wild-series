0fdd542469807f68828132948c776ce0eef1d181
Yoann PARDO-VINCENT <65440660+Yoyaug@users.noreply.github.com>
2020-11-24 21:56:25 +0100

    Method show Ok view show ok

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,15 +6,32 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class ProgramController
+ * @package App\Controller
+ * @Route("/programs", name="program_")
+ */
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/programs/", name="program_index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
+    }
+
+    /**
+     * @Route("/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="show")
+     * @param int $id
+     * @return Response
+     */
+    public function show(int $id): Response
+    {
+        return $this->render( 'show.html.twig', [
+            'id' => $id
+        ] );
     }
 }
