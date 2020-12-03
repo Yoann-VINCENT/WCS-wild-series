92a82159846f9d5fe437a5ff5bd45c405f3d7bce
yoann <yoann@LAPTOP-5JKAFLLK.localdomain>
2020-12-03 17:20:14 +0100

    NEw Category OK, new Program OK

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Program;
 use App\Entity\Category;
+use App\Form\CategoryType;
 
 /**
  * Class ProgramController
@@ -29,6 +31,30 @@ class CategoryController extends AbstractController
             'category/index.html.twig',
             ['categories' => $category]
         );
+    }
+
+    /**
+     * The controller for the category add form
+     * Display the form or deal with it
+     *
+     * @Route("/new", name="new")
+     * @param Request $request
+     * @return Response
+     */
+    public function new(Request $request) : Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($category);
+            $entityManager->flush();
+            return $this->redirectToRoute('category_index');
+        }
+        return $this->render('category/new.html.twig', [
+            "form" => $form->createView()
+        ]);
     }
 
     /**
